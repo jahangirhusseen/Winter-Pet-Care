f88e7a45684d4851ad925e4e48106c976419e2bb
Jahangir Hussen <ahmedjahangirid@gmail.com>
2025-11-28 18:36:16 +0600

    some update home pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,9 +7,6 @@ import WinterTips from "../components/WinterTips";
 const Home = () => {
   return (
     <>
-      <h1>Framer Motion</h1>
-      <h1>Subscribe Section</h1>
-      <h1>REadme file </h1>
       <Slider></Slider>
       <PopularSection />
       <MeetOurVets />
